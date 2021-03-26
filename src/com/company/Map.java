c6fdde89b6034ad9e2bba9accdb5fcaebbb15042
Zhandos-Hello-World <76952603+Zhandos-Hello-World@users.noreply.github.com>
2021-03-27 05:57:17 +0600

    I catch Exception
    But I don't know what send in the InvalidMapException

--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -6,12 +6,16 @@ public class Map{
     private int N;
     private char [][]NxN;
     Map(Scanner scanner) throws InvalidMapException{
+        boolean checkPosition = false;
         this.N = scanner.nextInt();
         NxN = new char[N][N];
         for(int y = 0; y < getSize(); y++){
             for(int x = 0; x < getSize(); x++){
                 char check = scanner.next().charAt(0);
                 if(check == '1' || check == '0' || check == 'P'){
+                    if(check == 'P'){
+                        checkPosition = true;
+                    }
                     NxN[y][x] = check;
                 }
                 else{
@@ -19,6 +23,11 @@ public class Map{
                 }
             }
         }
+        //here p is absent
+        if(!checkPosition){
+            throw new InvalidMapException("Hello p. How are you? Where are you?");
+        }
+
     }
     int getSize(){
         return this.N;
